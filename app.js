a19a84b457dc6678574e835b1ced5ce9c73a9b66
i6nake9 <fleshartemka@gmail.com>
2022-08-12 13:53:44 +0300

    Add public file

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const shopRouter = require('./routes/shop');
 app.use(shopRouter);
 app.use('/admin', adminRouter);
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use((req, res, next) => {
 	res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
