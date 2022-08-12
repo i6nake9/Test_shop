847c7885a9d414b15063737dff5f11e983a79962
i6nake9 <fleshartemka@gmail.com>
2022-08-12 17:04:43 +0300

    User plug

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const path = require('path');
-const app = express();
 const adminData = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 const bodyParser = require('body-parser');
+
+const app = express();
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(shopRouter);
 app.use('/admin', adminData.routes);
